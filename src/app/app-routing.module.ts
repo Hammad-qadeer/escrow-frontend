382e27cca17b74755164f45d1662507879fbf3e6
DESKTOP-FNVPSUH\Funav <tahirbsagri@gmail.com>
2023-06-13 11:55:52 +0500

    added icons on menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
-  // { path: '', redirectTo: 'home', pathMatch: 'full' },
   {path:'', loadChildren:()=>import('./layout/layout.module').then(m=>m.LayoutModule)},
   {path:'auth', loadChildren:()=>import('./user-management/user-management-routing.module').then(m=>m.UserManagementRoutingModule)},
   { path: '', redirectTo: '', pathMatch: 'full' },
